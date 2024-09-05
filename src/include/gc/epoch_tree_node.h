e3fb8e5ccd47515ebd3075e4f2065801542659d3
BonjunK00 <joonikoo10@gmail.com>
2024-09-05 14:20:36 +0900

    fix indent

--- src/include/gc/epoch_tree_node.h
+++ src/include/gc/epoch_tree_node.h
@@ -10,52 +10,52 @@ namespace gc {
 
 class EpochTreeNode {
 public:
-    virtual ~EpochTreeNode() = default;
-    virtual bool IsLeaf() const = 0;
+  virtual ~EpochTreeNode() = default;
+  virtual bool IsLeaf() const = 0;
 };
 
 class EpochTreeLeafNode : public EpochTreeNode {
 public:
-    EpochTreeLeafNode(eid_t epoch) : ref_count(0), epoch(epoch) {}
+  EpochTreeLeafNode(eid_t epoch) : ref_count(0), epoch(epoch) {}
 
-    int ref_count;
-    eid_t epoch;
-    std::vector<concurrency::TransactionContext*> transactions;
+  int ref_count;
+  eid_t epoch;
+  std::vector<concurrency::TransactionContext*> transactions;
 
-    EpochTreeLeafNode(int epoch) : ref_count(0), epoch(epoch) {}
+  EpochTreeLeafNode(int epoch) : ref_count(0), epoch(epoch) {}
 
-    void IncrementRefCount() {
-        ref_count++;
-    }
+  void IncrementRefCount() {
+    ref_count++;
+  }
 
-    void DecrementRefCount() {
-        if (ref_count > 0) ref_count--;
-    }
+  void DecrementRefCount() {
+    if (ref_count > 0) ref_count--;
+  }
 
-    bool IsGarbage() const {
-        return ref_count == 0;
-    }
+  bool IsGarbage() const {
+    return ref_count == 0;
+  }
 
-    bool IsLeaf() const override {
-        return true;
-    }
+  bool IsLeaf() const override {
+    return true;
+  }
 };
 
 class EpochTreeInternalNode : public EpochTreeNode {
 public:
-    eid_t epoch;
-    std::shared_ptr<EpochTreeNode> left;
-    std::shared_ptr<EpochTreeNode> right;
+  eid_t epoch;
+  std::shared_ptr<EpochTreeNode> left;
+  std::shared_ptr<EpochTreeNode> right;
 
-    EpochTreeInternalNode(eid_t epoch) : epoch(epoch), left(nullptr), right(nullptr) {}
+  EpochTreeInternalNode(eid_t epoch) : epoch(epoch), left(nullptr), right(nullptr) {}
 
-    EpochTreeLeafNode* FindLeafNode(eid_t epoch);
-    void InsertLeafNode(std::shared_ptr<EpochTreeLeafNode> EpochTreeNode);
-    void DeleteLeafNode(eid_t epoch);
+  EpochTreeLeafNode* FindLeafNode(eid_t epoch);
+  void InsertLeafNode(std::shared_ptr<EpochTreeLeafNode> EpochTreeNode);
+  void DeleteLeafNode(eid_t epoch);
 
-    bool IsLeaf() const override {
-        return false;
-    }
+  bool IsLeaf() const override {
+    return false;
+  }
 };
 
 }
