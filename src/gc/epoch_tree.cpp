1b115ade84907e70b5a717f0e1b0339bc74f2274
BonjunK00 <joonikoo10@gmail.com>
2024-09-21 22:33:30 +0900

    add insert epoch node

---
+++ src/gc/epoch_tree.cpp
@@ -0,0 +1,46 @@
+#include "gc/epoch_tree.h"
+
+namespace peloton {
+namespace gc {
+
+void EpochTree::InsertEpochNode(eid_t epoch) {
+  EpochLeafNode* new_leaf = new EpochLeafNode(epoch);
+  right_most_leaf = new_leaf;
+
+  if(root == nullptr) {
+    root = new_leaf;
+    return;
+  }
+
+  EpochNode* current = right_most_leaf;
+  while(current->parent != nullptr) {
+    EpochInternalNode* parent = dynamic_cast<EpochInternalNode*>(current->parent);
+    if(parent->left == nullptr) {
+      continue;
+    }
+    if(parent->right == nullptr || parent->right->size < parent->left->size) {
+      break;
+    }
+  }
+
+  eid_t start = current->IsLeaf() ? dynamic_cast<EpochLeafNode*>(current)->epoch 
+    : dynamic_cast<EpochInternalNode*>(current)->epoch_start;
+  EpochInternalNode* new_internal = new EpochInternalNode(start, epoch);
+  new_internal->right = new_leaf;
+  new_leaf->parent = new_internal;
+
+  if(current == root) {
+    root = new_internal;
+    new_internal->left = current;
+    current->parent = new_internal;
+    return;
+  }
+  
+  dynamic_cast<EpochInternalNode*>(current->parent)->right = new_internal;
+  new_internal->parent = current->parent;
+  new_internal->left = current;
+  current->parent = new_internal;
+}
+
+}
+}
--- src/include/gc/epoch_tree.h
+++ src/include/gc/epoch_tree.h
@@ -8,6 +8,10 @@ namespace gc {
 
 class EpochNode {
   public:
+    EpochNode* parent;
+    eid_t size;
+
+    EpochNode(eid_t size) : parent(nullptr), size(size) {}
     virtual ~EpochNode() = default;
     virtual bool IsLeaf() const = 0;
 };
@@ -16,13 +20,12 @@ class EpochInternalNode : public EpochNode {
  public:
   EpochNode* left;
   EpochNode* right;
-  EpochInternalNode* parent;
   eid_t epoch_start;
   eid_t epoch_end;
 
   EpochInternalNode(eid_t start, eid_t end) : 
-      left(nullptr), right(nullptr), parent(nullptr),
-      epoch_start(start), epoch_end(end) {}
+      left(nullptr), right(nullptr), 
+      epoch_start(start), epoch_end(end), EpochNode(end - start + 1) {}
   
   bool IsLeaf() const override {
     return false;
@@ -33,10 +36,9 @@ class EpochLeafNode : public EpochNode {
  public:
   eid_t epoch;
   int ref_count;
-  EpochInternalNode* parent;
   std::vector<concurrency::TransactionContext* > txns;
 
-  EpochLeafNode(eid_t epoch) : epoch(epoch), ref_count(0), parent(nullptr) {}
+  EpochLeafNode(eid_t epoch) : epoch(epoch), ref_count(0), EpochNode(1) {}
   
   bool IsLeaf() const override {
     return true;
