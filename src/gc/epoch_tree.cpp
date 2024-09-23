69cd9c8c91f13f77311d7cdca862b5c83bd99c63
BonjunK00 <joonikoo10@gmail.com>
2024-09-23 18:53:33 +0900

    update epoch tree error

--- src/gc/epoch_tree.cpp
+++ src/gc/epoch_tree.cpp
@@ -5,26 +5,33 @@ namespace gc {
 
 void EpochTree::InsertEpochNode(eid_t epoch) {
   EpochLeafNode* new_leaf = new EpochLeafNode(epoch);
-  right_most_leaf = new_leaf;
 
   if(root == nullptr) {
     root = new_leaf;
+    right_most_leaf = new_leaf;
     return;
   }
 
   EpochNode* current = right_most_leaf;
+
   while(current->parent != nullptr) {
     EpochInternalNode* parent = dynamic_cast<EpochInternalNode*>(current->parent);
+    
     if(parent->left == nullptr) {
       continue;
     }
+    
     if(parent->right == nullptr || parent->right->size < parent->left->size) {
       break;
     }
+
+    current = parent;
   }
 
-  eid_t start = current->IsLeaf() ? dynamic_cast<EpochLeafNode*>(current)->epoch 
+  eid_t start = current->IsLeaf() 
+    ? dynamic_cast<EpochLeafNode*>(current)->epoch 
     : dynamic_cast<EpochInternalNode*>(current)->epoch_start;
+
   EpochInternalNode* new_internal = new EpochInternalNode(start, epoch);
   new_internal->right = new_leaf;
   new_leaf->parent = new_internal;
@@ -33,6 +40,7 @@ void EpochTree::InsertEpochNode(eid_t epoch) {
     root = new_internal;
     new_internal->left = current;
     current->parent = new_internal;
+    right_most_leaf = new_leaf;
     return;
   }
   
@@ -40,6 +48,8 @@ void EpochTree::InsertEpochNode(eid_t epoch) {
   new_internal->parent = current->parent;
   new_internal->left = current;
   current->parent = new_internal;
+
+  right_most_leaf = new_leaf;
 }
 
 }
