d702b5f913ff9ef3d36313ef20a016297187a371
BonjunK00 <joonikoo10@gmail.com>
2024-10-04 16:50:37 +0900

    add memory test

---
+++ script/testing/htap/htap_memory_test.sh
@@ -0,0 +1,15 @@
+SESSION="my_session"
+
+./oltp_test_prepare.sh
+
+tmux new-session -d -s $SESSION
+tmux send-keys -t $SESSION "./oltp_test.sh" C-m
+
+tmux split-window -h
+tmux send-keys -t $SESSION "./transaction_begin.sh" C-m
+
+tmux split-window -h
+tmux send-keys -t $SESSION "./memory_test.sh" C-m
+
+tmux select-pane -t 0
+tmux -2 attach-session -t $SESSION
--- script/testing/htap/memory_test.sh
+++ script/testing/htap/memory_test.sh
@@ -2,14 +2,14 @@ PROCESS_NAME="peloton"
 OUTPUT_FILE="memory_usage.txt"
 INTERVAL=5
 
-echo "Timestamp, RSS (KB)" > $OUTPUT_FILE
+echo "Timestamp, RSS (KB)"
 
 while true; do
     TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
     MEMORY_USAGE=$(ps aux | grep $PROCESS_NAME | grep -v grep | awk '{print $6}')
     
     if [ ! -z "$MEMORY_USAGE" ]; then
-        echo "$TIMESTAMP, $MEMORY_USAGE" >> $OUTPUT_FILE
+        echo "$TIMESTAMP, $MEMORY_USAGE"
     fi
     
     sleep $INTERVAL
